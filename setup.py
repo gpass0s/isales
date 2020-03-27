4677a602f507a4793114ddaa1caa18e9ace60116
Guilherme Passos <guilhermepassos@outlook.com>
2020-03-27 15:35:37 -0300

    ENH: Add setup.py module to project

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 27 13:36 BRT 2020
+
+author: guilherme passos | twitter: @gpass0s
+"""
+from setuptools import setup
+
+
+setup(
+    name = "isales",
+    version = "0.0.0",
+    description = "Isales is an automated service that predicts leads probability to close",
+    author = "@gpass0s",
+    url = "https://github.com/gPass0s/isales"
+    packages = ["isales"]
+)
