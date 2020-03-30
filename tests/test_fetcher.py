f7e64d92fdc7a625647d5f9b9e5d3dc242b01bb7
Guilherme Passos <guilhermepassos@outlook.com>
2020-03-30 18:48:35 -0300

    ENH: Add four more test cases to test_fetcher modules

--- tests/test_fetcher.py
+++ tests/test_fetcher.py
@@ -1,37 +1,92 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Mar 29 10:59 BRT 
-
+Created on Sun Mar 29 10:59 BRT
+Last modified on Mon Mar 30 18:47 BRT
 author: guilherme passos | twitter: @gpass0s
 
 This module performs unit testing on fetcher module
 """
-
-from isales import fetcher 
-from isales.fetcher import DataFetcher
+from isales import fetcher
+from isales.fetcher import RedisReader, HubSpotFetcher
 from unittest.mock import MagicMock, patch
 
-_REDIS_QUEUE = [
-    b"[{'objectId': 1549880507, 'propertyName': 'closedate', 'propertyValue': '1587753210242'}]",
-    b"[{'objectId': 1398572107, 'propertyName': 'closed_lost_reason'}]"
+_SUBSCRIPTION_ITEMS = [
+    {'objectId': 1549880507, 'subscriptionType': 'deal.propertyChange'},
+    {'objectId': 383611195, 'subscriptionType': 'contact.creation'}
 ]
+
 
 @patch.dict("os.environ", {"MAX_BUFFER": "2"})
 @patch.dict("os.environ", {"QUEUE_TO_READ": "QUEUE_A"})
 @patch.object(fetcher, "RedisConnectionManager", autospec=True)
-def test_pull_from_redis(mocked_redis):
+def test_read_from_redis(mocked_redis_connection_manager):
 
     mocked_redis_client = MagicMock()
-    mocked_redis.return_value = mocked_redis_client
-
-    mocked_redis_client.lrange.return_value = _REDIS_QUEUE
-    expected_response = [
-        {"objectId": 1549880507, "propertyName": "closedate", "propertyValue": "1587753210242"},
-        {"objectId": 1398572107, "propertyName": "closed_lost_reason"}
+    mocked_redis_connection_manager.return_value = mocked_redis_client
+    redis_queue = [
+        b"[{'objectId': 1549880507, 'subscriptionType': 'deal.propertyChange'}]",
+        b"[{'objectId': 383611195, 'subscriptionType': 'contact.creation'}]"
     ]
+    mocked_redis_client.lrange.return_value = redis_queue
 
-    fetcher = DataFetcher()
-    response = fetcher._pull_from_redis()
+    redis_reader = RedisReader()
+    response = redis_reader()
 
-    assert expected_response == response
+    assert response == _SUBSCRIPTION_ITEMS
+
+
+@patch.dict("os.environ", {"DEAL_API_URL": "https://api.hubapi.com/deals/v1/deal/"})
+@patch.dict("os.environ", {"CONTACT_API_URL": "https://api.hubapi.com/contacts/v1/"})
+@patch.dict("os.environ", {"CONTACT_CREATION_SUBSCRIPTION": "contact.creation"})
+@patch.object(fetcher, "RedisConnectionManager", autospec=True)
+@patch.object(HubSpotFetcher, "_request")
+@patch.object(HubSpotFetcher, "_build_url")
+def test_fetch_from_hubspot(mocked_build_url, mocked_request, mocked_redis_connection_manager):
+
+    mocked_build_url.return_value = "https://api.hubapi.com/"
+    mocked_request.return_value = {"associations": {"associatedVids": [383216495]}}
+
+    hubspot_fetcher = HubSpotFetcher()
+    response = hubspot_fetcher(_SUBSCRIPTION_ITEMS)
+    expected_response = {
+        "new_contacts": [_SUBSCRIPTION_ITEMS[1]],
+        "deal_contacts": [{"associations": {"associatedVids": [383216495]}}]
+    }
+
+    assert response == expected_response
+
+
+@patch.dict("os.environ", {"DEAL_API_URL": "https://api.hubapi.com/deals/v1/deal/"})
+@patch.dict("os.environ", {"CONTACT_API_URL": "https://api.hubapi.com/contacts/v1/"})
+@patch.dict("os.environ", {"CONTACT_CREATION_SUBSCRIPTION": "contact.creation"})
+@patch.object(fetcher, "RedisConnectionManager", autospec=True)
+@patch.object(fetcher, "requests", autospec=True)
+def test_hubspot_fetcher_request(mocked_requests, mocked_redis_connection_manager):
+
+    mocked_redis_client = MagicMock()
+    mocked_redis_connection_manager.return_value = mocked_redis_client
+
+    hubspot_fetcher = HubSpotFetcher()
+    mocked_redis_client.get.return_value = b"ACCESS_TOKEN"
+    headers = {"Authorization": "Bearer ACCESS_TOKEN"}
+    url = "https://api.hubapi.com/contacts/v1/"
+    hubspot_fetcher._request(url)
+
+    mocked_requests.get.assert_called_once_with(url, headers=headers)
+
+
+@patch.dict("os.environ", {"DEAL_API_URL": "https://api.hubapi.com/deals/v1/deal/"})
+@patch.dict("os.environ", {"CONTACT_API_URL": "https://api.hubapi.com/contacts/v1/"})
+@patch.dict("os.environ", {"CONTACT_CREATION_SUBSCRIPTION": "contact.creation"})
+@patch.object(fetcher, "RedisConnectionManager", autospec=True)
+def test_hubspot_fetcher_build_contact_url(mocked_redis_connection_manager):
+
+    base_url = "https://api.hubapi.com/contacts/v1/vid="
+    ids = [1, 2, 3, 4, 5]
+
+    hubspot_fetcher = HubSpotFetcher()
+    response = hubspot_fetcher._build_url(base_url, *ids)
+    expected_response = "https://api.hubapi.com/contacts/v1/vid=1&vid=2&vid=3&vid=4&vid=5"
+
+    assert response == expected_response
