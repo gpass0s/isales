504e281e3261a2ce75f3d0bd544931e56a60ef61
Guilherme Passos <guilhermepassos@outlook.com>
2020-04-04 15:35:25 -0300

    ENH: Improve collector module for production

--- isales/collector.py
+++ isales/collector.py
@@ -2,7 +2,7 @@
 # -*- encoding: utf-8 -*-
 """
 Created on Sun Mar 29 08:25 BRT 2020
-Last modified on Fri 03 18:41 BRT 2020
+Last modified on Fri 04 15:34 BRT 2020
 author: guilherme passos | twitter: @gpass0s
 
 This module reads the new events that are arriving in a redis subscription queue, fetches these
@@ -10,6 +10,7 @@ events information from HubSpot database and writes the fetched data in another 
 """
 import json
 import os
+import re
 import requests
 
 from isales.logger import logger
@@ -25,43 +26,46 @@ class HubSpotFetcher:
         self.contact_api_url = os.environ["CONTACT_GET_API_URL"]
         self.contact_creation_subscription = os.environ["CONTACT_CREATION_SUBSCRIPTION"]
         self.predictable_contacts = os.environ["PREDICTABLE_CONTACTS_QUEUE"]
-        self.contacts_for_update_queue = os.environ["CONTATCS_FOR_UPDATE_QUEUE"]
+        self.contacts_for_update = os.environ["CONTATCS_FOR_UPDATE_QUEUE"]
 
-        max_buffer = os.environ["MAX_BUFFER"]
         if queue_to_read is None:
-            self.redis_reader = RedisReader(os.environ["QUEUE_TO_READ"], max_buffer)
+            self.queue_to_read = os.environ["QUEUE_TO_READ"]
         else:
-            self.redis_reader = RedisReader(queue_to_read, max_buffer)
+            self.queue_to_read = queue_to_read
 
+        max_buffer = os.environ["MAX_BUFFER_COLLETOR"]
+        self.redis_reader = RedisReader(self.queue_to_read, max_buffer)
         self.redis_writer = RedisWriter()
         self.contacts_id_to_fetch = []
         self.contacts = {
             self.predictable_contacts: [],
-            self.contacts_for_update_queue: [],
+            self.contacts_for_update: [],
         }
+        self.processed_items = {}
 
     def __call__(self):
         subscription_items = self.redis_reader()
         self.contacts[self.predictable_contacts].clear()
-        self.contacts[self.contacts_for_update_queue].clear()
+        self.contacts[self.contacts_for_update].clear()
+        self.contacts_id_to_fetch.clear()
+        self.processed_items = {}
         self._fetch_from_hubspot(subscription_items)
         if self.contacts_id_to_fetch:
             self._fetch_contact_info()
 
         self.redis_writer(self.contacts)
-        self.redis_reader.remove_items()
+        if self.processed_items:
+            self.redis_reader.remove_items()
 
     def _fetch_from_hubspot(self, subscription_items):
         """Fetches deals and contacts information from HubSpot database"""
-        self.contacts_id_to_fetch.clear()
-        processed_items = {}
         for item in subscription_items:
             subscription_type = item["subscriptionType"]
             object_name = item["subscriptionType"].split(".")[0]
 
             # better ask for forgiveness than permission
             try:
-                processed_items[item["objectId"]]
+                self.processed_items[item["objectId"]]
             except KeyError:
                 if subscription_type == self.contact_creation_subscription:
                     self._format_new_contact(item["objectId"])
@@ -73,12 +77,12 @@ class HubSpotFetcher:
                 if len(self.contacts_id_to_fetch) > 99:
                     self._fetch_contact_info()
 
-                processed_items[item["objectId"]] = True
+                self.processed_items[item["objectId"]] = True
 
     def _format_new_contact(self, contact_id):
         """Assigns prediction equals to 0 in contacts recently created"""
         new_contact = {"contact_id": contact_id, "prediction": "0"}
-        self.contacts[self.contacts_for_update_queue].append(new_contact)
+        self.contacts[self.contacts_for_update].append(new_contact)
 
     def _fetch_deal_info(self, deal_id):
         """ Fetches a deal information from Hubspot database"""
@@ -110,7 +114,34 @@ class HubSpotFetcher:
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
             return json.loads(response.text)
+        # Add contacts to redis again
+        if response.status_code == 429:
+            logger.error(
+                msg="429 - Ten sencondly rolling limit reached",
+                extra={"full_msg": response.text},
+            )
+            self._add_items_to_redis_again(url)
+
         return None
+
+    def _add_items_to_redis_again(self, url):
+        """ Add objets that got 429 requet error to redis again"""
+        is_contact = re.findall("contact", url)
+        if is_contact:
+            subscription_type = "contact.Error429"
+        else:
+            subscription_type = "deal.Error429"
+
+        objects_id = re.findall(r"\d+", url)[1:]
+        objects = []
+        for object_id in objects_id:
+            object_ = {
+                "objectId": int(object_id),
+                "subscriptionType": subscription_type,
+            }
+            objects.append(object_)
+        objects_wrapper = {self.queue_to_read: objects}
+        self.redis_writer(objects_wrapper)
 
     @staticmethod
     def _build_url(base_url, *ids_to_fetch):
