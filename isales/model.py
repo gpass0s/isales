8c1ead6718ee9459500e6f6e81d0b47ddf6d0800
Guilherme Passos <guilhermepassos@outlook.com>
2020-04-04 16:06:13 -0300

    WIP: Work in progress

---
+++ isales/model.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+"""
+Created on Sun on Fri 04 15:34 BRT 2020
+Last modified on Fri 04 15:34 BRT 2020
+author: guilherme passos | twitter: @gpass0s
+"""
